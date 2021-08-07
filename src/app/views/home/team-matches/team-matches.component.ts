116b5f48f0c8e62be20762a21e6f76d67c2938f5
Ahmed Elbaz <amahgoub@thiqah.sa>
2021-08-07 16:39:14 +0300

    ticket stracure

---
+++ src/app/views/home/team-matches/team-matches.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-team-matches',
+  templateUrl: './team-matches.component.html',
+  styleUrls: ['./team-matches.component.scss']
+})
+export class TeamMatchesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/views/reserve-ticket/reserve-ticket.component.ts
+++ src/app/views/reserve-ticket/reserve-ticket.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-reserve-ticket',
   templateUrl: './reserve-ticket.component.html',
-  styleUrls: ['./reserve-ticket.component.scss']
+  styleUrls: ['./reserve-ticket.component.scss'],
 })
 export class ReserveTicketComponent implements OnInit {
+  currentStep = 2;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
